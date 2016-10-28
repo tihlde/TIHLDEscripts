657e13a334dec81f8de077f8dc78920f0b2c4b2d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-10-28 12:04:27 +0200

    init commit

--- userscripts/ipascripts/userlib.py
+++ userscripts/ipascripts/userlib.py
@@ -2,9 +2,19 @@
 import random
 import string
 
+import pymysql
+
+from userscripts.ipascripts.ipahttp import ipa
+
 __author__ = 'Harald Floor Wilhelmsen'
 
+
 def generate_password(pwlen):
+    """
+    Generates a password with lower and upper case letters and numbers.
+    :param pwlen: Length of the password
+    :return: The generated password as a string
+    """
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     nums = string.digits
@@ -23,3 +33,57 @@ def generate_password(pwlen):
         pwindex = random.randint(0, pwlen - 1)
         pw[pwindex] = nums[num_index]
     return ''.join(pw)
+
+
+def get_ipa_api(uri='ipa1.tihlde.org'):
+    """
+    Create an api towards ipa with the given uri.
+    :param uri: Uri towards an ipa-server. Will be ipa1.tihlde.org if left at None
+    :return: The api
+    """
+    api = ipa(uri, sslverify=True)
+    # username, password(second line of ipa-admin password-file)
+    api.login('admin', open("/home/staff/drift/passord/ipa-admin").readlines()[1].replace('\n', '').strip())
+    return api
+
+
+def add_single_user(username, firstname, lastname, course, email, groupid, password=None, api=None):
+    """
+    Adds a single user to ipa and apache
+    :param password: The password of the new user. If null, one will be generated using @generate_password
+    :param api: API towards FREEIPA. This method assumes a login-request has been successful towards the IPA-server. \
+    If None, one will be created towards ipa1.tihlde.org, and a login-request will be sent
+    :return: The password and user id of the created user
+    """
+    if not api:
+        api = get_ipa_api()
+
+    if not password:
+        password = generate_password(12)
+
+    gecos = str(firstname) + ' ' + str(lastname) + ' ,' + str(email) + ' ,' + str(course)
+
+    info = \
+        {
+            "givenname": firstname,
+            "sn": lastname,
+            "userpassword": password,
+            "gecos": gecos,
+            "gidnumber": groupid,
+            "homedirectory": "/home/students/" + username
+        }
+
+    response = api.user_add(username, info)
+    error_response = response['error']
+    if error_response:
+        return
+    return [response['result']['result']['uidnumber'], password]
+
+
+def add_user_apache():
+    apache_db = pymysql.connect(host="localhost",
+                                user="apache",
+                                password=open("/home/staff/drift/passord/db-apache").readline().replace('\n', ''),
+                                database="apache",
+                                charset='utf8')
+    apache_cursor = apache_db.cursor()
