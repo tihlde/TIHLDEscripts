74b10694c75a5936db439be987620eeabe59378d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-10-31 12:39:51 +0100

    Added methods for sql-connections, adding users to apache and sending email

--- userscripts/ipascripts/userlib.py
+++ userscripts/ipascripts/userlib.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 import random
+import smtplib
 import string
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import pymysql
 
@@ -47,12 +50,12 @@ def get_ipa_api(uri='ipa1.tihlde.org'):
     return api
 
 
-def add_single_user(username, firstname, lastname, course, email, groupid, password=None, api=None):
+def add_user_ipa(username, firstname, lastname, course, email, groupid, password=None, api=None):
     """
     Adds a single user to ipa and apache
     :param password: The password of the new user. If null, one will be generated using @generate_password
     :param api: API towards FREEIPA. This method assumes a login-request has been successful towards the IPA-server. \
-    If None, one will be created towards ipa1.tihlde.org, and a login-request will be sent
+            If None, an ipa api object will be created towards ipa1.tihlde.org, and a login-request will be sent.
     :return: The password and user id of the created user
     """
     if not api:
@@ -80,10 +83,62 @@ def add_single_user(username, firstname, lastname, course, email, groupid, passw
     return [response['result']['result']['uidnumber'], password]
 
 
-def add_user_apache():
-    apache_db = pymysql.connect(host="localhost",
-                                user="apache",
-                                password=open("/home/staff/drift/passord/db-apache").readline().replace('\n', ''),
-                                database="apache",
-                                charset='utf8')
-    apache_cursor = apache_db.cursor()
+def mysql_connect(host, username, password, database_name, charset='utf8'):
+    return pymysql.connect(host=host,
+                           user=username,
+                           password=password,
+                           database=database_name,
+                           charset=charset)
+
+
+def add_user_apache(username, groupid, apache_conn=None, apache_cursor=None, commit_and_close=True):
+    """
+    Inserts a single user into the apache database
+    :param username: The username of the user to add to the database
+    :param groupid: Group id of the new user. In sql style. E. g. students us '7'
+    :param apache_conn: PyMySQL connection object. If None a new one will be created
+    :param apache_cursor: PyMySQL cursor-object. If None a new one will be
+    :param commit_and_close: Set to False if this method should NOT commit the query and
+            close the cursor and connection to the database. Default if True
+    :return: None if successful. An error-message if not.
+    """
+    if not apache_cursor:
+        if not apache_conn:
+            apache_pw = open("/home/staff/drift/passord/db-apache").readline().rstrip('\n')
+            apache_conn = mysql_connect("localhost", "apache", apache_pw, "apache")
+        apache_cursor = apache_conn.cursor()
+    try:
+        apache_cursor.execute(
+            "INSERT INTO `apache`.`brukere` (`id`, `brukernavn`, `gruppe`, `expired`, `deaktivert`, `webdav`, `kommentar`) "
+            "VALUES (NULL, '{0}', '{1}', 'false', 'false', 'false', '');".format(username, str(groupid)))
+        if commit_and_close:
+            apache_conn.commit()
+            apache_cursor.close()
+            apache_conn.close()
+        return
+    except pymysql.Error as err:
+        return 'Error when pushing user {0} to the apache database:\n{1}' \
+            .format(username, err)
+
+
+def send_email(recipient, subject, body, sender='drift@tihlde.org', smtp_host='localhost'):
+    """
+    Sends an email with the given data to the given recipient.
+    :param recipient: Recipient email address
+    :param subject: Subject of the email
+    :param body: Body of the email
+    :param sender: Email address of the sender
+    :param smtp_host: Host to send the email with. Standard is 'localhost'
+    :return: None if successful. Error-msg if not.
+    """
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+    text = msg.as_string()
+    try:
+        smtpObj = smtplib.SMTP(smtp_host)
+        smtpObj.sendmail(sender, recipient, text)
+    except smtplib.SMTPException as error:
+        return 'Error: unable to send email to "{0}". Error-msg:\n{1}'.format(recipient, error)
